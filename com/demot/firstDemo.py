46ee084900a2635337c34eca4144133713d8e356
Marco Willgren <mhw1@hotmail.com>
2015-03-17 02:23:22 +0200

    python script for creating (child) processes

--- com/demot/firstDemo.py
+++ com/demot/firstDemo.py
@@ -1,21 +1,22 @@
+#!/usr/bin/python
+
 import os
 
 if __name__ == '__main__':
-    pass
+        pass
 
-def child():
-    print 'A new child ',  os.getpid( )
-    os._exit(0)  
+def fork():
+        for i in range(3):
+                newProcessId = os.fork()
+                if newProcessId == 0:
+                        os._exit(0)
+                else:
+                        processIds = (newProcessId, os.getpid(), os.getpgid(newProcessId))
+                        #print "parentID: %d, ProcessID: %d, process groupID: %d" % processIds
 
-def parent():
-    while True:
-        newpid = os.fork()
-        if newpid == 0:
-            child()
-        else:
-            pids = (os.getpid(), newpid)
-            print "parent: %d, child: %d" % pids
-            if raw_input( ) == 'q': break
+                        with open(str(newProcessId) + '.txt', 'w') as fout:
+                                fout.write('ProcessID: ' + str(processIds[0]) + '\n')
+                                fout.write('ParentID: ' + str(processIds[1]) + '\n')
+                                fout.write('Process groupID: ' + str(processIds[2]) + '\n')
 
-
-parent()
+fork()
